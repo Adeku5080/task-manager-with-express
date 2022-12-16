c4f561199755132b43d72c60d5c330a1c9493754
Ali Adeku <aliadeku.aam@gmail.com>
2022-12-16 16:36:35 +0100

    Write tests for the get a task and delete a task endpoint

--- tests/integration/tasksController.test.js
+++ tests/integration/tasksController.test.js
@@ -26,10 +26,77 @@ describe("Task Controller", () => {
         .get("/api/v1/tasks")
         .set("content-type", "application/json");
 
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("tasks");
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("tasks");
+    });
+  });
+
+  describe("get a task", () => {
+    let task;
+    beforeEach(async () => {
+      task = await TaskModel.create({
+        name: "task testid",
+      });
+    });
+    it("should return 200 and a single task", async () => {
+      const response = await request(app)
+        .get(`/api/v1/tasks/${task.id}`)
+        .set("content-type", "application/json");
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("task");
+    });
+  });
+
+  describe("update a task", () => {
+    let task;
+    beforeEach(async () => {
+      task = await TaskModel.create({
+        name: "task2 testid",
+      });
     });
 
- 
+    it("should return 404 if the task with the id doesnt exist", async () => {
+      const taskId = "639c80ef98284bfdf111ad09";
+      const response = await request(app).delete(`/api/v1/tasks/${taskId}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.msg).toEqual("this task does not exist");
+    });
+     
+
+    it("should return 200 and the updated task", async () => {
+      const response = await request(app)
+        .patch(`/api/v1/tasks/${task.id}`)
+        .send({ name: "newtask" });
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("task");
+    });
+  });
+
+  describe("delete a task", () => {
+    let task;
+    beforeEach(async () => {
+      task = await TaskModel.create({
+        name: "task2 testid",
+      });
+    });
+
+    it("should return 404 if the task with the id doesnt exist", async () => {
+      const taskId = "639c80ef98284bfdf111ad09";
+      const response = await request(app).delete(`/api/v1/tasks/${taskId}`);
+
+      expect(response.status).toBe(404);
+
+      expect(response.body.msg).toEqual("this task does not exist");
+    });
+
+    it("should return 200 and the deleted task", async () => {
+      const response = await request(app).delete(`/api/v1/tasks/${task.id}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("task");
+    });
   });
 });
